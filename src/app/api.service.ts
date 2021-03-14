7a2687194f58343667218dc691250f5790b40c79
supergillie <peter.krooks@gmail.com>
2021-03-14 19:46:04 +0100

    errorhandling

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 
-import {  throwError } from 'rxjs';
+import { throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 
 
